9aa7f63a384d04d66de2fd7df1463e5917feec7e
Chris Lorenzo <Christopher_Lorenzo@Comcast.com>
2022-05-03 21:16:00 -0400

    WIP: Add cypress tests

---
+++ cypress/integration/tmdb.spec.js
@@ -0,0 +1,19 @@
+describe('Lightning TMDB app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/')
+  })
+
+  it('displays a browse page with rows', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get('.todo-list li').should('have.length', 2)
+
+    // We can go even further and check that the default todos each contain
+    // the correct text. We use the `first` and `last` functions
+    // to get just the first and last matched elements individually,
+    // and then perform an assertion with `should`.
+    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  })
+})
